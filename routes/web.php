a613eb52d2cdcefbf172af53c8dd458c9a9cec96
unknown <kalyosnmartins@gmail.com>
2025-01-06 13:18:31 -0400

    Push inicial

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,5 @@ Route ::get('/about', function(){
     echo "About Us";
 
 });
+
+Route ::get('/main', [MainController::class, 'index']);
